da9f4c7daed66395a107d773cf2edd44d93f74c6
Yentl Hendrickx <r0842908@student.thomasmore.be>
2023-07-02 17:41:34 +0200

    init

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <>
+            <nav>
+
+
+                <p>Navbar Here</p>
+            </nav>
+            <div>
+                <p>Here comes top cool bar thingy</p>
+            </div>
+        </>
+    );
+}
+
+export default Header;
